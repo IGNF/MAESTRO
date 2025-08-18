3cc4e775c930548e615092c1a1e9570addac8733
Antoine Labatie <antoine.labatie@gmail.com>
2025-08-18 18:05:26 +0200

    test: add tests

---
+++ tests/test_data.py
@@ -0,0 +1,43 @@
+import itertools
+
+import pytest
+
+from ssl_models import LOGGER
+from ssl_models.train.data import SSLDataModule
+
+
+@pytest.mark.parametrize(
+    "stage, ssl_phase, use_transform, random_dates, random_crop",
+    itertools.product(
+        ("fit", "validate"),
+        ("pretrain", "probe", "finetune"),
+        (True, False),
+        (True, False),
+        (True, False),
+    ),
+)
+def test_data(
+    datasets_treesat,
+    opt_finetune,
+    stage,
+    ssl_phase,
+    use_transform,
+    random_dates,
+    random_crop,
+):
+    data_module = SSLDataModule(
+        datasets=datasets_treesat,
+        ssl_phase=ssl_phase,
+        opt=opt_finetune,
+        num_workers=1,
+        use_transform=use_transform,
+        random_dates=random_dates,
+        random_crop=random_crop,
+    )
+    data_module.setup(stage=stage)
+    data_loader = data_module.val_dataloader()
+    num_epochs = 2
+    for _ in range(num_epochs):
+        for idx, batch in enumerate(data_loader):
+            LOGGER.info(idx)
+            LOGGER.info(f"id: {idx}, batch: {batch.keys()}")
---
+++ tests/test_model.py
@@ -0,0 +1,112 @@
+import itertools
+
+import pytest
+
+from ssl_models.train.model import SSLModule
+
+
+@pytest.mark.parametrize(
+    "multimodal, allmods_depth, stage, model_size",
+    itertools.product(
+        ("shared", "monotemp", "mod", "group"),
+        (0,),
+        ("train", "val", "test"),
+        ("tiny",),
+    ),
+)
+def test_ssl_mae(
+    datasets_treesat,
+    mask,
+    multimodal,
+    allmods_depth,
+    stage,
+    model_size,
+):
+    ssl_module = SSLModule(
+        datasets=datasets_treesat,
+        mask=mask,
+        multimodal=multimodal,
+        allmods_depth=allmods_depth,
+        model="mae",
+        model_size=model_size,
+    )
+    ssl_module.setup(stage=stage)
+
+
+@pytest.mark.parametrize(
+    "multimodal, allmods_depth, stage, model_size",
+    itertools.product(
+        ("mod", "group"),
+        (3,),
+        ("train", "val", "test"),
+        ("tiny",),
+    ),
+)
+def test_ssl_allmods_mae(
+    datasets_treesat,
+    mask,
+    multimodal,
+    allmods_depth,
+    stage,
+    model_size,
+):
+    ssl_module = SSLModule(
+        datasets=datasets_treesat,
+        mask=mask,
+        multimodal=multimodal,
+        allmods_depth=allmods_depth,
+        model="mae",
+        model_size=model_size,
+    )
+    ssl_module.setup(stage=stage)
+
+
+@pytest.mark.parametrize(
+    "multimodal, allmods_depth, stage, model_size, type_head, loss",
+    itertools.product(
+        ("group",),
+        (0,),
+        ("train", "val", "test"),
+        ("tiny",),
+        ("attentive", "linear"),
+        ("l1_norm", "l1"),
+    ),
+)
+def test_ssl_head_loss(
+    datasets_treesat,
+    mask,
+    multimodal,
+    allmods_depth,
+    stage,
+    model_size,
+    type_head,
+    loss,
+):
+    ssl_module = SSLModule(
+        datasets=datasets_treesat,
+        mask=mask,
+        multimodal=multimodal,
+        allmods_depth=allmods_depth,
+        model="mae",
+        model_size=model_size,
+        type_head=type_head,
+        loss=loss,
+    )
+    ssl_module.setup(stage=stage)
+
+
+@pytest.mark.xfail
+def test_model_should_fail(datasets_treesat, mask):
+    _ = SSLModule(
+        datasets=datasets_treesat,
+        mask=mask,
+        multimodal="group",
+        allmods_depth=0,
+        model="MAE",
+        model_size="tiny",
+    )
+
+
+@pytest.mark.xfail
+def test_missing_args_should_fail():
+    _ = SSLModule(model="mae", model_size="tiny")

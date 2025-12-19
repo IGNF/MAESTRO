5505f0d0ebe7844477ccfc619742de4f1b93c6ad
Antoine Labatie <antoine.labatie@gmail.com>
2025-12-19 14:17:03 +0100

    chore: remove unused mask option

--- conf/mask.py
+++ conf/mask.py
@@ -8,7 +8,6 @@ from hydra_zen import store
 @dataclass
 class MaskConfig:  # noqa: D101
     mask_ratio: float = 0.75
-    nb_mix_images: int = 4
     mask_scale: float = 0.0
     mask_mod: float | None = 0.25
     mask_bands: float | None = None
